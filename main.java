a7655d05089eda3d7ee8e41b50c09925370f78a3
Eugene Colarte Cabiao <146933792+Eugene-Colarte-Cabiao@users.noreply.github.com>
2023-10-22 15:08:19 +0800

    Version 2

--- main.java
+++ main.java
@@ -1,10 +1,45 @@
-From my repository folder, I'm going to upload this to my github repository.
+public class Main {
+    public static void main(String[] args) {
 
+        String name = "Eugene Colarte Cabiao";
+        int age = 22;
+        double perHr = 169.69;
 
-"an hour ago"
+        System.out.println("Complete Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Per Hour : " + perHr);
 
+        System.out.println("****************************************");
 
+        double SSS, PAG_IBIG, PHIC, DAILY_EXPENSES;
 
+        SSS = 110.19;
+        PAG_IBIG = 149.75;
+        PHIC = 209.69;
+        DAILY_EXPENSES = 300.50;
 
-YES! I MADE IT! 
-Next is I will modify this file and upload it again to my github repository, but this time its gonna be Version 1.1
+        double deductions = SSS + PAG_IBIG + PHIC + DAILY_EXPENSES;
+
+        int total_hr = 24;
+
+        double total_salary = perHr * total_hr;
+
+        double NIGHT_PREMIUM, DOUBLE_PAY;
+
+        NIGHT_PREMIUM = 350.69;
+        DOUBLE_PAY = 600.69;
+
+        double Additional_Salary = NIGHT_PREMIUM + DOUBLE_PAY;
+
+        double GROSS = total_salary + Additional_Salary;
+
+        System.out.println("Total Hour Render : " + total_hr);
+        System.out.println("Gross Salary : " + GROSS);
+        System.out.println("Deductions : " + deductions);
+
+        System.out.println("****************************************");
+
+        double NET = GROSS - deductions;
+        System.out.println("Net Salary : " + NET);
+    }
+}
